92279247d73e809c3964a976163db25eaf326e7b
Danvace <vipdan055@gmail.com>
2023-08-09 16:59:08 +0300

    add some annotation for better performing and method getStudentById

--- src/main/java/com/danylo/braslavets/studentRating/Ratingsystem/model/Group.java
+++ src/main/java/com/danylo/braslavets/studentRating/Ratingsystem/model/Group.java
@@ -1,7 +1,11 @@
 package com.danylo.braslavets.studentRating.Ratingsystem.model;
 
+import com.danylo.braslavets.studentRating.Ratingsystem.exception.StudentNotFoundException;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
@@ -14,12 +18,17 @@ public class Group {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Group name should not be null")
+    @NotEmpty(message = "Group name should not be empty")
     private String groupName;
+
     @OneToOne
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Student groupLeader;
 
     @JsonManagedReference
     @OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private List<Student> students;
 
     public void addStudent(Student student) {
@@ -30,6 +39,16 @@ public class Group {
     public void removeStudent(Student student) {
         students.remove(student);
         student.setGroup(null);
+    }
+
+    public Student getStudentById(Long id) throws StudentNotFoundException {
+        return students.stream()
+                .filter(student -> student.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new StudentNotFoundException(
+                        String.format("There is no student with id = %s in group %s",
+                                id,
+                                groupName)));
     }
 
     public double groupRating() {
