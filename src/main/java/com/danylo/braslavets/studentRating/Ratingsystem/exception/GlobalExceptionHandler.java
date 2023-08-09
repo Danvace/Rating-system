3477d9e46f052f9a883e5bd52d4719aef2a10e4e
Danvace <vipdan055@gmail.com>
2023-08-09 18:56:34 +0300

    add method to handle validation exceptions and NotFoundExceptions

--- src/main/java/com/danylo/braslavets/studentRating/Ratingsystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/danylo/braslavets/studentRating/Ratingsystem/exception/GlobalExceptionHandler.java
@@ -2,16 +2,24 @@ package com.danylo.braslavets.studentRating.Ratingsystem.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(StudentNotFoundException.class)
-    public ResponseEntity<ErrorDetails> handleStudentNotFoundException(StudentNotFoundException e) {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleNotFoundException(NotFoundException e) {
         ErrorDetails errorDetails = new ErrorDetails(e.getMessage(), HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        ErrorDetails errorDetails = new ErrorDetails(errorMessage, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
 }
